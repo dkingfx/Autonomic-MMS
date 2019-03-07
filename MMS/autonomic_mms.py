b1fd670a03e79fde3eed7c6d5bbc8dca8c10d7c6
Daniel <daniel@osx-6.local>
2019-03-07 00:11:20 -0500

    updates

--- MMS/autonomic_mms.py
+++ MMS/autonomic_mms.py
@@ -26,29 +26,16 @@ tn.write(command + b"\n")
 print("Success!")
 tn.close()
 
-<<<<<<< HEAD
 class ConnectPort23:
     """Control Autonomic-MMS via port 23"""
-=======
-class ControlOverPort23:
-    """Connect to MMS via port 23"""
->>>>>>> c70bdd0e0678c6bc42b07c8e70343ce204ba5eae
     class systemCommands(Enum):
         """Valid port 23 commands"""
         UPTIME = 'Uptime'
-        BROWSECLIENT = 'BrowseClients'
-        BROWSESOURCE = 'BrowseSources'
-        UPDATE = 'AutoUpdate'
         REBOOT = 'Reboot'
         SHUTDOWN = 'Shutdown'
 
-<<<<<<< HEAD
 class ConnectPort5004:
     """Control Autonomic-MMS via port 5004"""
-=======
-class ControlOverPort5004:
-    """Connect to MMS via port 5004"""
->>>>>>> c70bdd0e0678c6bc42b07c8e70343ce204ba5eae
     class controlCommands(Enum):
         """Valid port 5004 commands"""
         NOWPLAYING = 'BrowseNowPlaying'
@@ -70,22 +57,3 @@ class ControlOverPort5004:
         REWIND = 'Rewind'
         FASTFORWARD = 'FastForward'
         GETART = 'GetArt'
-<<<<<<< HEAD
-=======
-
-class controlOverPort5006:
-    """Connect to MMS via port 5006"""
-    class MradControlCommands(Enum):
-        """Valid port 5006 commands"""
-        MRADALLOFF = 'AllOff'
-        MRADBROWSEALLSOURCE = 'BrowseAllSources'
-        MRADBROWSEALLZONES = 'BrowseAllZones'
-        MRADBROUSESOURCE = 'BrowseSources'
-        MRADBROWSEZONEGRP = 'BrowseZoneGroup'
-        MRADBROWSEZONEGRPS = 'BrowseZoneGroups'
-        MRADBRWOSEZONES = 'BrowseZones'
-        MRADSTATUS = 'GetStatus'
-        MRADMUTEALL = 'MuteAll'
-        MRADPOWER = 'Power'
-      
->>>>>>> c70bdd0e0678c6bc42b07c8e70343ce204ba5eae
